aa2c91ca9eae59675a3fbfdbe64d9e527cfcf955
tomjyc <tomjyc@bigmir.net>
2016-11-01 11:00:02 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), #означает, что Django возьмет все, что придется на эту часть строки и передаст представлению в качестве переменной pk , [0-9] означает, что допустимы только цифры (от 0 до 9), но не буквы , + означает, что цифр должно быть от одной и больше , pk сокращение от primary key
 ]
