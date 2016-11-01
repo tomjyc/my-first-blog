594798eee05efabf9e4f4b133bd72a112e4afd04
tomjyc <tomjyc@bigmir.net>
2016-11-01 14:00:25 +0200

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), #означает, что Django возьмет все, что придется на эту часть строки и передаст представлению в качестве переменной pk , [0-9] означает, что допустимы только цифры (от 0 до 9), но не буквы , + означает, что цифр должно быть от одной и больше , pk сокращение от primary key
-]
+        url(r'^$', views.post_list, name='post_list'),
+        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    ]
